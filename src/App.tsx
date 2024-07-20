ee3892d5d7c2be22ce957e39ac55e77244460c5e
Sara Vahidipour <m.vahidipour>
2024-07-20 12:32:00 +0330

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import "./App.css";
 import SignalR from "./components/signalR";
+import Welcome from "./pages/welcome";
+import {  BrowserRouter,Route, Routes } from "react-router-dom";
 
 function App() {
-  return <SignalR />;
+  return (
+  <BrowserRouter>
+  <div>
+    <Routes>
+      <Route path="/" element={<Welcome/>}/>
+    </Routes>
+  </div>
+  </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/welcome.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Welcome() {
+  return (
+    <div style={{ marginTop: "5vh" , display: 'ruby-text',height:'100vh',maxWidth: '600px',textAlign: 'center',
+        alignContent: 'center',backgroundColor: 'antiquewhite',position: 'relative',
+        margin: '0% auto 0',minHeight:'100vh'}}>
+        <p>
+        welcome
+        </p>
+    </div>
+  );
+}
