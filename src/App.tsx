5d63d1cbb4744b3e8f9b67aebbdf51c8118c7e0d
m.abaraghi <abaraghi.mina@golrangleasing.com>
2024-07-20 12:49:08 +0330

    common mode;

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import "./App.css";
-import SignalR from "./components/signalR";
-import Welcome from "./pages/welcome";
+import './App.css';
+import SignalR from './components/signalR';
+import Welcome from './pages/welcome';
 import {useSelector} from 'react-redux';
 import {RootState} from './redux/store';
 import {Link} from 'react-router-dom';
-import {  BrowserRouter,Route, Routes } from "react-router-dom";
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import React from 'react';
 function App() {
-  const signalR = useSelector((state: RootState) => state.msgSlice.msg);
-  return (
-  <BrowserRouter>
-  <div>
-    <Routes>
-      <Route path="/" element={<Welcome/>}/>
-    </Routes>
-    <SignalR />
-            <p>{signalR}</p>
-            <Link to="/auth/forgot-password">mm</Link>
-  </div>
-  </BrowserRouter>
-  );
+    const signalR = useSelector((state: RootState) => state.msgSlice.msg);
+    return (
+        <BrowserRouter>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Welcome />} />
+                </Routes>
+                <SignalR />
+                <p>{signalR}</p>
+                <Link to="/auth/forgot-password">mm</Link>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/models/models.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Models() {
-  return (
-    <div>models</div>
-  )
-}
