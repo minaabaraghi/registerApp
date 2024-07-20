34832e0a93a2d33d2a1174ad98b3ab459e5624d2
m.abaraghi <abaraghi.mina@golrangleasing.com>
2024-07-20 12:29:26 +0330

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import "./App.css";
-import SignalR from "./components/signalR";
-
+import './App.css';
+import React from 'react';
+import SignalR from './components/signalR';
+import {useSelector} from 'react-redux';
+import {RootState} from './redux/store';
+import {Link} from 'react-router-dom';
 function App() {
-  return <SignalR />;
+    const signalR = useSelector((state: RootState) => state.msgSlice.msg);
+    return (
+        <>
+            <SignalR />
+            <p>{signalR}</p>
+            <Link to="/auth/forgot-password">mm</Link>
+        </>
+    );
 }
 
 export default App;
--- src/redux/reciveMSG/reciveSlice.tsx
+++ src/redux/reciveMSG/reciveSlice.tsx
@@ -1,26 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from '@reduxjs/toolkit';
 
 interface reciveMsg {
-  msg: string;
+    msg: string[];
 }
 
 const initialState: reciveMsg = {
-  msg: "",
+    msg: [''],
 };
 
 export const msgSlice = createSlice({
-  name: "message",
-  initialState,
-  reducers: {
-    msgAction: (state: any, action: any) => {
-      const { msg } = action.payload;
-      state.msg = msg;
+    name: 'message',
+    initialState,
+    reducers: {
+        msgAction: (state: any, action: any) => {
+            const {msg} = action.payload;
+            state.msg = msg;
+        },
     },
-  },
 });
 
 // this is for dispatch
-export const { msgAction } = msgSlice.actions;
+export const {msgAction}: any = msgSlice.actions;
 
 // this is for configureStore
 export default msgSlice.reducer;
