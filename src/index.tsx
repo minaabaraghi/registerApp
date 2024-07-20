88a11dcaf11b9e33e960150e67c511787bff7941
m.abaraghi <abaraghi.mina@golrangleasing.com>
2024-07-20 11:07:34 +0330

    query

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import reportWebVitals from './reportWebVitals'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
+import {QueryClientProvider, QueryClient} from 'react-query';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 0,
+            refetchOnWindowFocus: false,
+            cacheTime: Infinity,
+        },
+    },
+});
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-)
+    <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </QueryClientProvider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister()
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
