3de8a9751ba95dd0f02983813eef36ba7354cb72
m.abaraghi <abaraghi.mina@golrangleasing.com>
2024-07-23 11:35:19 +0330

    theme and dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Welcome from './pages/welcome';
-
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import BasePage from './routes/basePage';
-
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 function App() {
+    const [mode, setMode] = useState(true);
+    const darkTheme = createTheme({
+        palette: {
+            mode: mode ? 'dark' : 'light',
+        },
+    });
     return (
         <div
             style={{
@@ -22,9 +30,26 @@ function App() {
             }}
             className="App"
         >
-            <BrowserRouter>
-                <BasePage />
-            </BrowserRouter>
+            {!mode ? (
+                <DarkModeIcon
+                    onClick={() => {
+                        setMode(true);
+                    }}
+                />
+            ) : (
+                <LightModeIcon
+                    onClick={() => {
+                        setMode(false);
+                    }}
+                />
+            )}
+
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
+                <BrowserRouter>
+                    <BasePage />
+                </BrowserRouter>
+            </ThemeProvider>
         </div>
     );
 }
--- src/sample/dropDown/dropDown.tsx
+++ src/sample/dropDown/dropDown.tsx
@@ -1,3 +1,50 @@
-export default function DropDown() {
-    return <></>;
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+export default function TemporaryDrawer() {
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setOpen(newOpen);
+    };
+
+    const [label, setLabel] = React.useState('');
+    return (
+        <div>
+            <FormControl fullWidth>
+                <Button
+                    onClick={toggleDrawer(true)}
+                    startIcon={<KeyboardArrowDownIcon />}
+                    fullWidth
+                    sx={{background: 'e1e1e1', width: '50px'}}
+                >
+                    {label}
+                </Button>
+            </FormControl>
+            <Drawer anchor={'bottom'} open={open} onClose={toggleDrawer(false)}>
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemText
+                                    primary={text}
+                                    onClick={() => setLabel(text)}
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </div>
+    );
 }
